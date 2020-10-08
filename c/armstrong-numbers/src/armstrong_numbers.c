d01e1aa0ef38791776ce55a117440e8b2f57d6db
Leland Krych <leland.krych@gmail.com>
2020-10-07 22:05:38 -0700

    a little cleverer way to do it

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -2,18 +2,14 @@
 #include <math.h>
 
 bool is_armstrong_number(int candidate) {
-    int copy_of_candidate, digit;
+    int copy_of_candidate, digit, ans;
     int ndigits = 0;
     double armstrong = 0;
 
     copy_of_candidate = candidate;
 
-    while (copy_of_candidate > 0) {
-        copy_of_candidate = copy_of_candidate / 10;
-        ndigits += 1;
-    }
+    ndigits = floor(log10(candidate)) + 1;
 
-    copy_of_candidate = candidate;
     while (copy_of_candidate > 0) {
         digit = copy_of_candidate % 10;
 
@@ -21,8 +17,6 @@ bool is_armstrong_number(int candidate) {
         armstrong += pow(digit,ndigits);
     }
 
-    if (candidate == armstrong) {
-        return true;
-    }
-    return false;
+    ans = candidate == armstrong ? true : false;
+    return ans;
 }

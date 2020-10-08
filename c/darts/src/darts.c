c65726f40aa2421369e13e88d5d5080492fe697e
Leland Krych <leland.krych@gmail.com>
2020-10-08 07:33:59 -0700

    fix compilation issues by adding typedef for the coordinate_t struct type

--- c/darts/src/darts.c
+++ c/darts/src/darts.c
@@ -1,1 +1,5 @@
 #include "darts.h"
+
+int score(struct coordinate_t dart) {
+    return 0;
+}
--- c/darts/src/darts.h
+++ c/darts/src/darts.h
@@ -1,4 +1,11 @@
 #ifndef DARTS_H
 #define DARTS_H
 
+typedef struct coordinate_t {
+    float x;
+    float y;
+} coordinate_t;
+
+int score(coordinate_t dart);
+
 #endif

2f52ffbd2a94ddd4a7aacddf0524874eda32ba1b
Leland Krych <leland.krych@gmail.com>
2020-11-10 07:24:48 -0800

    pass first test

--- c/rna-transcription/src/rna_transcription.c
+++ c/rna-transcription/src/rna_transcription.c
@@ -1,1 +1,33 @@
 #include "rna_transcription.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char bio_translation(char c) {
+    c = toupper(c);
+    switch(c) {
+        case 'G':
+            return 'C';
+        case 'C':
+            return 'G';
+        case 'T':
+            return 'A';
+        case 'A':
+            return 'U';
+        default:
+            printf("%c is an invalid DNA nucleotide -- Skipping", c);
+            return ' ';
+    }
+}
+
+char *to_rna(const char *dna) {
+    if (dna == NULL) {
+        return NULL;
+    }
+    char* rna = (char*) malloc(strlen(dna));
+    for(int i = 0; i < (int)strlen(dna); i++) {
+        rna[i] = bio_translation(dna[i]);
+    }
+    return rna;
+}

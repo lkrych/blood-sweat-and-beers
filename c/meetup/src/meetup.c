204f5a074b8e95708135cfaaf883dd4e37532ede
Leland Krych <leland.krych@gmail.com>
2020-11-13 07:36:13 -0800

    initial commit and pass first test

---
+++ c/meetup/src/meetup.c
@@ -0,0 +1,60 @@
+#include "meetup.h"
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+
+#define TEENTH 9
+
+const char * weekday[] = { "Sunday", "Monday",
+                             "Tuesday", "Wednesday",
+                             "Thursday", "Friday", "Saturday"};
+
+int get_count(const char* week) {
+    if (strcmp(week, "first") == 0) {
+        return 1;
+    } else if (strcmp(week, "second") == 0) {
+        return 2;
+    } else if (strcmp(week, "third") == 0) {
+        return 3;
+    } else if (strcmp(week, "fourth") == 0) {
+        return 4;
+    } else if (strcmp(week, "teenth") == 0) {
+        // nonsense number we can use as switch
+        return TEENTH;
+    }
+    return 5;
+}
+
+
+int meetup_day_of_month(unsigned int year, unsigned int month, const char *week, const char *day_of_week) {
+    int month_for_time_struct = month - 1;
+    struct tm t = { .tm_year=year-1900, .tm_mon=month_for_time_struct, .tm_mday=1 };
+    int day_of_week_count = 0;
+    int day_of_month = 1;
+    int needed_count = get_count(week);
+    printf("=====================================\n");
+    printf("searching for %d, %s\n", needed_count, day_of_week);
+    printf("=====================================\n");
+    int current_month = (int) month_for_time_struct;
+    while (current_month == (int) month_for_time_struct) {
+        const char* current_day = weekday[t.tm_wday];
+        printf("current day is %s, current count is %d\n", current_day, day_of_week_count);
+        if (current_day == day_of_week) {
+            day_of_week_count += 1;
+            if (needed_count == TEENTH && day_of_month >= 13 && day_of_month <= 19) {
+                // found the teenth day
+                break;
+            }
+        }
+        if (day_of_week_count == needed_count) {
+            break;
+        }
+
+        // keep marching along
+        t.tm_mday += 1;
+        mktime(&t);
+        day_of_month += 1;
+        current_month = t.tm_mon;
+    }
+    return day_of_month;
+}

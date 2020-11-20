9a76b2b6215f2456b9956ec0ee6dff8cfd356b23
Leland Krych <leland.krych@gmail.com>
2020-11-20 07:11:06 -0800

    refactor to be a bit cleaner

--- c/grains/src/grains.c
+++ c/grains/src/grains.c
@@ -2,22 +2,17 @@
 #include <stdio.h>
 #include <math.h>
 
-#define CHESS_SQUARES 64
+#define MAX 64
+#define MIN 1
 
 uint64_t square(uint8_t index) {
-    if (index <= 0) {
+    if (index < MIN || index > MAX ) {
         return 0;
     }
-    if (index > CHESS_SQUARES) {
-        return 0;
-    }
-    return pow(2, index - 1);
+
+    return powl(2, index - 1);
 }
 
 uint64_t total(void) {
-    uint64_t total = 0;
-    for (int i = 0; i < CHESS_SQUARES +1; i++) {
-        total += square(i);
-    }
-    return total;
+    return powl(2, 64) - 1;
 }

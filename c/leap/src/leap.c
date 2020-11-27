13b69e57aff7857e9236925cad317ae3a411e07b
Leland Krych <leland.krych@gmail.com>
2020-11-27 12:03:32 -0800

    refactor to evaluate expressionn once

--- c/leap/src/leap.c
+++ c/leap/src/leap.c
@@ -4,13 +4,13 @@ bool is_leap_year(int year){
     bool divis_by_4 = (year % 4) == 0;
     bool divis_by_100 = (year % 100) == 0;
     bool divis_by_400 = (year % 400) == 0;
-    if (divis_by_4 && divis_by_100 && divis_by_400) {
-        return true;
-    }
-    if (divis_by_4 && divis_by_100) {
-        return false;
-    }
     if (divis_by_4) {
+        if (divis_by_100) {
+            if (divis_by_400) {
+                return true;
+            }
+            return false;
+        }
         return true;
     }
     return false;
